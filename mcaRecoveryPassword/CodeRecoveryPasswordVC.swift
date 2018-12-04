bb5eee7d2f2f56b244d66de11e351e582d6e6416
pil4r <pilarr.pz@gmail.com>
2018-12-03 18:26:27 -0600

    REcovery password view added

---
+++ mcaRecoveryPassword/CodeRecoveryPasswordVC.swift
@@ -0,0 +1,30 @@
+//
+//  CodeRecoveryPasswordVC.swift
+//  mcaRecoveryPassword
+//
+//  Created by Pilar del Rosario Prospero Zeferino on 12/3/18.
+//  Copyright © 2018 Speedy Movil. All rights reserved.
+//
+
+import UIKit
+
+class CodeRecoveryPasswordVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ mcaRecoveryPassword/mcaRecoveryPassword/mcaRecoveryPassword.h
@@ -0,0 +1,19 @@
+//
+//  mcaRecoveryPassword.h
+//  mcaRecoveryPassword
+//
+//  Created by Pilar del Rosario Prospero Zeferino on 12/3/18.
+//  Copyright © 2018 Speedy Movil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for mcaRecoveryPassword.
+FOUNDATION_EXPORT double mcaRecoveryPasswordVersionNumber;
+
+//! Project version string for mcaRecoveryPassword.
+FOUNDATION_EXPORT const unsigned char mcaRecoveryPasswordVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <mcaRecoveryPassword/PublicHeader.h>
+
+
